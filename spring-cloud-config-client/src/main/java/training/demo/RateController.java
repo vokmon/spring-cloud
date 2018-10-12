1d6db7fac58b72127cddfd6b2c5ed1baadf23508
Arnon Ruangthanawes <arnon.raungthanawes@unico.com.au>
2018-10-12 16:09:11 +1100

    Update resouce on the server does not need to restart the app by calling /actuator/refresh

--- spring-cloud-config-client/src/main/java/training/demo/RateController.java
+++ spring-cloud-config-client/src/main/java/training/demo/RateController.java
@@ -4,6 +4,7 @@
 package training.demo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author aruangth
  */
 @Controller
+@RefreshScope // Refresh properties
 public class RateController {
 
     @Value("${rate}")
