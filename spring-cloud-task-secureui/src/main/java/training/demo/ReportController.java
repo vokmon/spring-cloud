dbe6c7bce3211fce2b1d4e1b5c8cc53794b37428
Arnon Ruangthanawes <arnon.raungthanawes@unico.com.au>
2018-10-15 12:03:09 +1100

    Add security ui project

---
+++ spring-cloud-task-secureui/src/main/java/training/demo/ReportController.java
@@ -0,0 +1,29 @@
+/**
+ *
+ */
+package training.demo;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author aruangth
+ */
+@Controller
+@EnableOAuth2Sso
+public class ReportController extends WebSecurityConfigurerAdapter {
+
+    @RequestMapping("/")
+    public String loadHome() {
+        return "home";
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers(
+                "/", "/login**").permitAll().anyRequest().authenticated();
+    }
+}
---
+++ spring-cloud-task-secureui/src/main/java/training/demo/SpringCloudTaskSecureuiApplication.java
@@ -0,0 +1,12 @@
+package training.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudTaskSecureuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudTaskSecureuiApplication.class, args);
+	}
+}
