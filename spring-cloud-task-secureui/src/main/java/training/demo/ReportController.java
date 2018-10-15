301e862c55f6cbadd3909b14a9ea9673ad48c362
Arnon Ruangthanawes <arnon.raungthanawes@unico.com.au>
2018-10-15 12:32:37 +1100

    Add security ui project

--- spring-cloud-task-secureui/src/main/java/training/demo/ReportController.java
+++ spring-cloud-task-secureui/src/main/java/training/demo/ReportController.java
@@ -3,10 +3,20 @@
  */
 package training.demo;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -16,14 +26,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @EnableOAuth2Sso
 public class ReportController extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private OAuth2ClientContext clientContext;
+
+    @Autowired
+    private OAuth2RestTemplate oauth2ResTemplate;
+
     @RequestMapping("/")
     public String loadHome() {
         return "home";
+    }
+
+    @RequestMapping("/reports")
+    public String loadReqports(Model model) {
+        final OAuth2AccessToken token = clientContext.getAccessToken();
+        System.out.println("Token: " + token.getValue());
+
+        final ResponseEntity<ArrayList<TollUsage>> tolls = oauth2ResTemplate.exchange(
+                "http://localhost:9001/services/tolldata", HttpMethod.GET, null,
+                new ParameterizedTypeReference<ArrayList<TollUsage>>() {
+                });
+        model.addAttribute("tolls", tolls.getBody());
+        return "reports";
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(
                 "/", "/login**").permitAll().anyRequest().authenticated();
+    }
+
+    public static class TollUsage {
+
+        public String Id;
+        public String stationId;
+        public String licensePlate;
+        public String timestamp;
+
+        public TollUsage() {
+        }
+
+        public TollUsage(String id, String stationid, String licenseplate, String timestamp) {
+            this.Id = id;
+            this.stationId = stationid;
+            this.licensePlate = licenseplate;
+            this.timestamp = timestamp;
+        }
+
     }
 }
